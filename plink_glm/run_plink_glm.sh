0268bf3648fcf40420aaf005a391d8f2eaf6f87c
LiliiaBgdnv <109213422+LiliiaBgdnv@users.noreply.github.com>
2023-06-07 13:28:29 +0300

    update results folder and scripts

--- plink_glm/run_plink_glm.sh
+++ plink_glm/run_plink_glm.sh
@@ -9,17 +9,21 @@ cut -f2- ../Raw_data/phenotypes_simple_trait_gen.tsv > ../Raw_data/phenotypes_si
 
 # Run plink2
 # simple trait
-../plink2 --glm allow-no-covars --allow-extra-chr --bed ./inputs/simple_trait.bed --bim ./inputs/simple_trait.bim --fam ./inputs/simple_trait.fam --pheno ../Raw_data/phenotypes_simple_trait_2col.tsv --adjust cols='chrom','pos','alt','a1','ref','gc','fdrbh' --out ./results/simple_glm_result_chr --covar-variance-standardize --freq --threads 32 --memory 100000
-awk '{gsub(/^GLYMAchr_/,""); print}' ./results/simple_glm_result_chr.Leu.glm.linear.adjusted  > ./results/simple_glm_result_chr.Leu.glm.linear.adjusted
-awk '{gsub(/^GLYMAchr_/,""); print}' ./results/simple_glm_result_chr.Leu.glm.linear > plink_glm/results/simple_glm_result_chr.Leu.glm.linear
+../plink2 --glm allow-no-covars --allow-extra-chr --bed ./inputs/simple_trait.bed --bim ./inputs/simple_trait.bim --fam ./inputs/simple_trait.fam --pheno ../Raw_data/phenotypes_simple_trait_2col.tsv --adjust cols='chrom','pos','alt','a1','ref','gc','fdrbh' --out ../results/plink_glm/simple_glm_result_chr --covar-variance-standardize --freq --threads 32 --memory 100000
+awk '{gsub(/^GLYMAchr_/,""); print}' ../results/plink_glm/simple_glm_result_chr.Leu.glm.linear.adjusted  > ../results/plink_glm/simple_glm_result.Leu.glm.linear.adjusted
+awk '{gsub(/^GLYMAchr_/,""); print}' ../results/plink_glm/simple_glm_result_chr.Leu.glm.linear > ../results/plink_glm/simple_glm_result.Leu.glm.linear
 
 # complex trait
-../plink2 --glm allow-no-covars --allow-extra-chr --bed ./inputs/complex_trait.bed --bim ./inputs/complex_trait.bim --fam ./inputs/complex_trait.fam --pheno ../Raw_data/phenotypes_complex_trait.tsv --adjust --out ./results/complex_glm_result --covar-variance-standardize --freq --threads 32 --memory 100000
+../plink2 --glm allow-no-covars --allow-extra-chr --bed ./inputs/complex_trait.bed --bim ./inputs/complex_trait.bim --fam ./inputs/complex_trait.fam --pheno ../Raw_data/phenotypes_complex_trait.tsv --adjust --out ../results/plink_glm/complex_glm_result --covar-variance-standardize --freq --threads 32 --memory 100000
 
 # simple trait generated
-../plink2 --glm allow-no-covars --allow-extra-chr --bed ./inputs/simple_trait.bed --bim ./inputs/simple_trait.bim --fam ./inputs/simple_trait.fam --pheno ../Raw_data/phenotypes_simple_trait_2col_gen.tsv --adjust cols='chrom','pos','alt','a1','ref','gc','fdrbh' --out ./results/simple_glm_gen_result_chr --covar-variance-standardize --freq --threads 32 --memory 100000
-awk '{gsub(/^GLYMAchr_/,""); print}' ./results/simple_glm_gen_result_chr.Leu.glm.linear.adjusted  > ./results/simple_glm_gen_result_chr.Leu.glm.linear.adjusted
-awk '{gsub(/^GLYMAchr_/,""); print}' ./results/simple_glm_gen_result_chr.Leu.glm.linear > ./results/simple_glm_gen_result_chr.Leu.glm.linear
+../plink2 --glm allow-no-covars --allow-extra-chr --bed ./inputs/simple_trait.bed --bim ./inputs/simple_trait.bim --fam ./inputs/simple_trait.fam --pheno ../Raw_data/phenotypes_simple_trait_2col_gen.tsv --adjust cols='chrom','pos','alt','a1','ref','gc','fdrbh' --out ../results/plink_glm/simple_glm_gen_result_chr --covar-variance-standardize --freq --threads 32 --memory 100000
+awk '{gsub(/^GLYMAchr_/,""); print}' ../results/plink_glm/simple_glm_gen_result_chr.Leu.glm.linear.adjusted  > ../results/plink_glm/simple_glm_gen_result.Leu.glm.linear.adjusted
+awk '{gsub(/^GLYMAchr_/,""); print}' ../results/plink_glm/simple_glm_gen_result_chr.Leu.glm.linear > ../results/plink_glm/simple_glm_gen_result.Leu.glm.linear
 
 # complex trait generated
-../plink2 --glm allow-no-covars --allow-extra-chr --bed ./inputs/complex_trait.bed --bim ./inputs/complex_trait.bim --fam ./inputs/complex_trait.fam --pheno ../Raw_data/phenotypes_complex_trait_gen.tsv --adjust --out ./results/complex_glm_gen_result --covar-variance-standardize --freq --threads 32 --memory 100000
+../plink2 --glm allow-no-covars --allow-extra-chr --bed ./inputs/complex_trait.bed --bim ./inputs/complex_trait.bim --fam ./inputs/complex_trait.fam --pheno ../Raw_data/phenotypes_complex_trait_gen.tsv --adjust --out ../results/plink_glm/complex_glm_gen_result --covar-variance-standardize --freq --threads 32 --memory 100000
+
+#
+rm ../results/plink_glm/*PHENO1*
+rm ../results/plink_glm/simple_glm_result_chr.Leu.glm.linear.adjusted ../results/plink_glm/simple_glm_result_chr.Leu.glm.linear ../results/plink_glm/simple_glm_gen_result_chr.Leu.glm.linear.adjusted ../results/plink_glm/simple_glm_gen_result_chr.Leu.glm.linear
